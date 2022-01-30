69f6768ddef8aa69cff17c7fa176df534ab308b9
eminarican <emin.arican@outlook.com>
2022-01-30 19:13:31 +0300

    xbox info && chunk status

---
+++ src/pocketmine/player/UsedChunkStatus.hx
@@ -0,0 +1,13 @@
+package pocketmine.player;
+
+@:native("pocketmine\\player\\UsedChunkStatus")
+extern final class UsedChunkStatus {
+
+    static function NEEDED(): UsedChunkStatus;
+
+    static function REQUESTED_GENERATION(): UsedChunkStatus;
+
+    static function REQUESTED_SENDING(): UsedChunkStatus;
+
+    static function SENT(): UsedChunkStatus;
+}
---
+++ src/pocketmine/player/XboxLivePlayerInfo.hx
@@ -0,0 +1,13 @@
+package pocketmine.player;
+
+@:native("pocketmine\\player\\XboxLivePlayerInfo")
+extern final class XboxLivePlayerInfo extends PlayerInfo {
+
+    public function getXuid(): String;
+
+	/**
+	 * Returns a new PlayerInfo with XBL player info stripped. This is used to ensure that non-XBL players can't spoof
+	 * XBL data.
+	 */
+	public function withoutXboxData(): PlayerInfo;
+}

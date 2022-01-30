2067b2036bb41b0939a6dadc5f74a6969e840c7c
eminarican <emin.arican@outlook.com>
2022-01-30 17:53:30 +0300

    plugin

---
+++ src/pocketmine/plugin/Plugin.hx
@@ -0,0 +1,32 @@
+package pocketmine.plugin;
+
+@:native("pocketmine\\plugin\\Plugin")
+extern interface Plugin {
+    
+    function isEnabled(): Bool;
+
+	/**
+	 * Called by the plugin manager when the plugin is enabled or disabled to inform the plugin of its enabled state.
+	 *
+	 * @internal This is intended for core use only and should not be used by plugins
+	 * @see PluginManager::enablePlugin()
+	 * @see PluginManager::disablePlugin()
+	 */
+	function onEnableStateChange(enabled: Bool): Void;
+
+	/**
+	 * Gets the plugin's data folder to save files and configuration.
+	 * This directory name has a trailing slash.
+	 */
+	function getDataFolder(): String;
+
+	//function getDescription(): PluginDescription;
+
+	function getName(): String;
+
+	//function getLogger(): AttachableLogger;
+
+	//function getPluginLoader(): PluginLoader;
+
+	//function getScheduler(): TaskScheduler;
+}
--- src/pocketmine/plugin/PluginBase.hx
+++ src/pocketmine/plugin/PluginBase.hx
@@ -1,9 +1,80 @@
 package pocketmine.plugin;
 
+import php.files.SplFileInfo;
+import php.Resource;
+
 @:native("pocketmine\\plugin\\PluginBase")
-extern abstract class PluginBase {
+extern abstract class PluginBase implements Plugin {
 
-    function onEnable(): Void;
+    function onLoad(): Void;
 
-    function onDisable(): Void;
+	/**
+	 * Called when the plugin is enabled
+	 */
+	function onEnable(): Void;
+
+	/**
+	 * Called when the plugin is disabled
+	 * Use this to free open things and finish actions
+	 */
+	function onDisable(): Void;
+
+	final function isEnabled(): Bool;
+
+	/**
+	 * Called by the plugin manager when the plugin is enabled or disabled to inform the plugin of its enabled state.
+	 *
+	 * @Internal This is Intended for core use only and should not be used by plugins
+	 * @see PluginManager::enablePlugin()
+	 * @see PluginManager::disablePlugin()
+	 */
+	final function onEnableStateChange(enabled: Bool): Void;
+
+	final function isDisabled(): Bool;
+
+	final function getDataFolder(): String;
+
+	//final function getDescription(): PluginDescription;
+
+	//function getLogger(): AttachableLogger;
+
+	//function getCommand(name: String): Null<EitherType<Command, PluginOwned>>;
+
+	//function onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Bool;
+
+	/**
+	 * Gets an embedded resource on the plugin file.
+	 * WARNING: You must close the resource given using fclose()
+	 */
+	function getResource(filename: String): Null<Resource>;
+
+	/**
+	 * Saves an embedded resource to its relative location in the data folder
+	 */
+	function saveResource(filename: String, replace: Bool = false): Bool;
+
+	/**
+	 * Returns all the resources packaged with the plugin in the form ["path/in/resources" => SplFileInfo]
+	 */
+	function getResources(): Array<SplFileInfo>;
+
+	//function getConfig(): Config;
+
+	function saveConfig(): Void;
+
+	function saveDefaultConfig(): Bool;
+
+	function reloadConfig(): Void;
+
+	final function getServer(): Server;
+
+	final function getName(): String;
+
+	final function getFullName(): String;
+
+	function getFile(): String;
+
+	//function getPluginLoader(): PluginLoader;
+
+	//function getScheduler(): TaskScheduler;
 }

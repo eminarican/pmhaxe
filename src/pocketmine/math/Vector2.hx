58acef02d51b706d61e230ee923a443b61f25eda
eminarican <emin.arican@outlook.com>
2022-01-30 20:07:29 +0300

    vectors

---
+++ src/pocketmine/math/Vector2.hx
@@ -0,0 +1,49 @@
+package pocketmine.math;
+
+@:native("pocketmine\\math\\Vector2")
+extern class Vector2 {
+
+    function new(x: Float, y: Float): Void;
+
+	function getX(): Float;
+
+	function getY(): Float;
+
+	function getFloorX(): Int;
+
+	function getFloorY(): Int;
+
+	function add(x: Float, y: Float): Vector2;
+
+	function addVector(vector2: Vector2): Vector2;
+
+	function subtract(x: Float, y: Float): Vector2;
+
+	function subtractVector(vector2: Vector2): Vector2;
+
+	function ceil(): Vector2;
+
+	function floor(): Vector2;
+
+	function round(): Vector2;
+
+	function abs(): Vector2;
+
+	function multiply(number: Float): Vector2;
+
+	function divide(number: Float): Vector2;
+
+	function distance(pos: Vector2): Float;
+
+	function distanceSquared(pos: Vector2): Float;
+
+	function length(): Float;
+
+	function lengthSquared(): Float;
+
+	function normalize(): Vector2;
+
+	function dot(v: Vector2): Float;
+
+	function __toString(): String;
+}
---
+++ src/pocketmine/math/VectorMath.hx
@@ -0,0 +1,7 @@
+package pocketmine.math;
+
+@:native("pocketmine\\math\\VectorMath")
+extern class VectorMath {
+
+    static function getDirection2D(azimuth: Float): Vector2;
+}

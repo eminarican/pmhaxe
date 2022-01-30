9587af29cc467331f0df89c556cdbf3c2f721002
eminarican <emin.arican@outlook.com>
2022-01-30 21:59:16 +0300

    binary stream && limits

---
+++ src/pocketmine/utils/BinaryStream.hx
@@ -0,0 +1,201 @@
+package pocketmine.utils;
+
+@:native("pocketmine\\utils\\BinaryStream")
+extern class BinaryStream {
+    
+    function new(buffer: String = "", offset: Int = 0);
+
+	/**
+	 * Rewinds the stream poInter to the start.
+	 */
+	function rewind(): Void;
+
+	function setOffset(offset: Int): Void;
+
+	function getOffset(): Int;
+
+	function getBuffer(): String;
+
+	/**
+	 * @throws BinaryDataException if there are not enough bytes left in the buffer
+	 */
+	function get(len: Int): String;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getRemaining(): String;
+
+	function put(str: String): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getBool(): Bool;
+
+	function putBool(v: Bool): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getByte(): Int;
+
+	function putByte(v: Int): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getShort(): Int;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getSignedShort(): Int;
+
+	function putShort(v: Int): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getLShort(): Int;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getSignedLShort(): Int;
+
+	function putLShort(v: Int): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getTriad(): Int;
+
+	function putTriad(v: Int): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getLTriad(): Int;
+
+	function putLTriad(v: Int): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getInt(): Int;
+
+	function putInt(v: Int): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getLInt(): Int;
+
+	function putLInt(v: Int): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getFloat(): Float;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getRoundedFloat(accuracy: Int): Float;
+
+	function putFloat(v: Float): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getLFloat(): Float;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getRoundedLFloat(accuracy: Int): Float;
+
+	function putLFloat(v: Float): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getDouble(): Float;
+
+	function putDouble(v: Float): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getLDouble(): Float;
+
+	function putLDouble(v: Float): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getLong(): Int;
+
+	function putLong(v: Int): Void;
+
+	/**
+	 * @throws BinaryDataException
+	 */
+	function getLLong(): Int;
+
+	function putLLong(v: Int): Void;
+
+	/**
+	 * Reads a 32-bit variable-length unsigned Integer from the buffer and returns it.
+	 *
+	 * @throws BinaryDataException
+	 */
+	function getUnsignedVarInt(): Int;
+
+	/**
+	 * Writes a 32-bit variable-length unsigned Integer to the end of the buffer.
+	 */
+	function putUnsignedVarInt(v: Int): Void;
+
+	/**
+	 * Reads a 32-bit zigzag-encoded variable-length Integer from the buffer and returns it.
+	 *
+	 * @throws BinaryDataException
+	 */
+	function getVarInt(): Int;
+
+	/**
+	 * Writes a 32-bit zigzag-encoded variable-length Integer to the end of the buffer.
+	 */
+	function putVarInt(v: Int): Void;
+
+	/**
+	 * Reads a 64-bit variable-length Integer from the buffer and returns it.
+	 *
+	 * @throws BinaryDataException
+	 */
+	function getUnsignedVarLong(): Int;
+
+	/**
+	 * Writes a 64-bit variable-length Integer to the end of the buffer.
+	 */
+	function putUnsignedVarLong(v: Int): Void;
+
+	/**
+	 * Reads a 64-bit zigzag-encoded variable-length Integer from the buffer and returns it.
+	 *
+	 * @throws BinaryDataException
+	 */
+	function getVarLong(): Int;
+
+	/**
+	 * Writes a 64-bit zigzag-encoded variable-length Integer to the end of the buffer.
+	 */
+	function putVarLong(v: Int): Void;
+
+	/**
+	 * Returns whether the offset has reached the end of the buffer.
+	 */
+	function feof(): Bool;
+}
---
+++ src/pocketmine/utils/Limits.hx
@@ -0,0 +1,22 @@
+package pocketmine.utils;
+
+@:native("pocketmine\\utils\\Limits")
+extern final class Limits {
+
+    inline static final UINT8_MAX = 0xff;
+	inline static final INT8_MIN = -0x7f - 1;
+	inline static final INT8_MAX = 0x7f;
+
+	inline static final UINT16_MAX = 0xffff;
+	inline static final INT16_MIN = -0x7fff - 1;
+	inline static final INT16_MAX = 0x7fff;
+
+	inline static final UINT32_MAX = 0xffffffff;
+	inline static final INT32_MIN = -0x7fffffff - 1;
+	inline static final INT32_MAX = 0x7fffffff;
+
+    // Todo: find out fix
+	//inline static final UINT64_MAX = 0xffffffffffffffff;
+	inline static final INT64_MIN = -0x7fffffffffffffff - 1;
+	//inline static final INT64_MAX = 0x7fffffffffffffff;
+}
